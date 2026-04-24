78d03f96382e9ee0a5a1fe2469da369c41c8ce29
MateusM312 <mateusmendes2626@gmail.com>
2026-04-24 11:37:52 -0300

    admissao + desligamentos

--- adm/tabelas-criar/headcount/headcount-script.js
+++ adm/tabelas-criar/headcount/headcount-script.js
@@ -12,25 +12,59 @@ function close_sidebar() {
 }
 
 function porcentage_totalF() {
-    const total = document.getElementById('total').innerText;
+    const total_txt = document.getElementById('total').innerText;
     const howmanyvar = document.getElementById('howmany').innerText;
-    let now_porcentage = (howmanyvar * 100) / total;
+    let now_porcentage = (howmanyvar * 100) / total_txt;
 
     document.getElementById('total-%').innerText = now_porcentage.toFixed(1) + '%';
     document.getElementById('barra-cheia').style.width = now_porcentage + '%';
 }
 
-function editar(i){
+function admissao() {
+  let total = Number(document.getElementById('total').innerText);
+  let mes = Number(document.getElementById('mes').innerText);
+
+  let resultado = (mes / total) * 100;
+  document.getElementById('admissao').innerText = '+' + resultado.toFixed(1) + '%';
+  let barra = document.getElementById('barrames').style.width = resultado + '%';
+}
+
+function desligamentos(){
+  let admissoes = Number(document.getElementById('mes').innerText);
+  let desliga = Number(document.getElementById('deslig').innerText);
+  let total = Number(document.getElementById('total').innerText);
+
+  let saldo = admissoes - desliga;
+  let now_porcen = (desliga / total) * 100;
+
+  document.getElementById('saldo').innerText = '+' + saldo.toFixed(0);
+  document.getElementById('barradeslig').style.width = now_porcen + '%';
+}
+
+function editar(i) {
   const valorAtual = i.textContent;
   const input = document.createElement('input');
   input.value = valorAtual;
   input.className = i.className;
 
-  input.onblur = () =>{
-      i.textContent = input.value || valorAtual;
-      input.replaceWith(i);
-      porcentage_totalF();
+  input.onblur = () => {
+    const novoValor = input.value || valorAtual;
+    i.textContent = novoValor;
+    input.replaceWith(i);
+
+    if (i.id === 'total') {
+      total = Number(novoValor); // usa novoValor, não input.value
+      porcentage_totalF();               // recalcula depois de atualizar total
+    }
+    if (i.id === 'mes') {       // edita 'mes', não 'admissao'
+      mes = Number(novoValor);
+      admissao();               // recalcula e atualiza o campo admissao
+    }
+    if(i.id === 'deslig'){
+      desligamentos();
+    }
   };
+
 
   input.onkeydown = (e) => {
       if (e.key === "Enter") input.blur();
@@ -42,5 +76,7 @@ function editar(i){
 }
 
 window.onload = function(){
+  admissao();
   porcentage_totalF();
+  desligamentos();
 }
