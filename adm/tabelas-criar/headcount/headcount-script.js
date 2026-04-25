cbbccdffd39db7d0e9a404746bd30f4f57d79627
MateusM312 <mateusmendes2626@gmail.com>
2026-04-25 14:50:27 -0300

    barra atualizacoes | total func | admissoes no mes

--- adm/tabelas-criar/headcount/headcount-script.js
+++ adm/tabelas-criar/headcount/headcount-script.js
@@ -11,72 +11,150 @@ function close_sidebar() {
   document.getElementById("openav").style.display = "inline-block";
 }
 
-function porcentage_totalF() {
-    const total_txt = document.getElementById('total').innerText;
-    const howmanyvar = document.getElementById('howmany').innerText;
-    let now_porcentage = (howmanyvar * 100) / total_txt;
+let total_func, ativos_total, admissoes_total;
 
-    document.getElementById('total-%').innerText = now_porcentage.toFixed(1) + '%';
-    document.getElementById('barra-cheia').style.width = now_porcentage + '%';
+
+function add_total_func(){
+  const inputs = document.querySelectorAll('.somar-func');
+  const inputs_ativos = document.querySelectorAll('.ativosT');
+
+  const total__ativos = Array.from(inputs_ativos).reduce((sum, input) => {
+    return sum + (Number(input.value) || 0);
+  }, 0);
+
+  const total = Array.from(inputs).reduce((sum, input) => {
+    return sum + (Number(input.value) || 0);
+  }, 0);
+
+  ativos_total = total__ativos;
+  total_func = total;
 }
 
-function admissao() {
-  let total = Number(document.getElementById('total').innerText);
-  let mes = Number(document.getElementById('mes').innerText);
+function add_total_adimissoes(){
+  const admissoes = document.querySelectorAll('.admissoesT');
 
-  let resultado = (mes / total) * 100;
-  document.getElementById('admissao').innerText = '+' + resultado.toFixed(1) + '%';
-  let barra = document.getElementById('barrames').style.width = resultado + '%';
+  const total_admissoes = Array.from(admissoes).reduce((sum, input) => {
+    return sum + (Number(input.value) || 0);
+  }, 0);
+
+  admissoes_total = total_admissoes;
 }
 
-function desligamentos(){
-  let admissoes = Number(document.getElementById('mes').innerText);
-  let desliga = Number(document.getElementById('deslig').innerText);
-  let total = Number(document.getElementById('total').innerText);
+function atualizar_board_admissao(){
+  const num_mes = Number(document.getElementById('mes').innerText);
+  const barra = document.getElementById('barrames');
 
-  let saldo = admissoes - desliga;
-  let now_porcen = (desliga / total) * 100;
+  let porcentagem_barra = (admissoes_total / total_func) * 100;
+  const porcen_segura = isFinite(porcentagem_barra) ? porcentagem_barra : 0;
 
-  document.getElementById('saldo').innerText = '+' + saldo.toFixed(0);
-  document.getElementById('barradeslig').style.width = now_porcen + '%';
+  document.getElementById('admissao').innerText = porcen_segura.toFixed(1) + '%';
+  document.getElementById('barrames').style.width = porcen_segura + '%';
+  document.getElementById('mes').innerText = admissoes_total || 0;
 }
 
-function editar(i) {
-  const valorAtual = i.textContent;
-  const input = document.createElement('input');
-  input.value = valorAtual;
-  input.className = i.className;
+document.getElementById('barra-cheia').style.width = 0 + '%';
 
-  input.onblur = () => {
-    const novoValor = input.value || valorAtual;
-    i.textContent = novoValor;
-    input.replaceWith(i);
+function atualizar_total_func_board(){
+  const total = Number(document.getElementById('total').innerText);
+  const barra_func = document.getElementById('barra-cheia').style.width;
 
-    if (i.id === 'total') {
-      total = Number(novoValor); // usa novoValor, não input.value
-      porcentage_totalF();               // recalcula depois de atualizar total
-    }
-    if (i.id === 'mes') {       // edita 'mes', não 'admissao'
-      mes = Number(novoValor);
-      admissao();               // recalcula e atualiza o campo admissao
-    }
-    if(i.id === 'deslig'){
-      desligamentos();
-    }
-  };
+  const barra = document.getElementById('barra-cheia').style.width;
 
+  const varios_ativos = Number(document.getElementById('howmany'));
+  const porcentagem = (ativos_total * 100) / total_func;
+  const porcentagemSegura = isFinite(porcentagem) ? porcentagem : 0;
 
-  input.onkeydown = (e) => {
-      if (e.key === "Enter") input.blur();
-  };
+  document.getElementById('total-%').innerText = porcentagemSegura.toFixed(1) + '%';
+  document.getElementById('barra-cheia').style.width = porcentagem + '%';
+  document.getElementById('howmany').innerText = ativos_total || 0;
+  document.getElementById('total').innerText = total_func || 0;
+  
+  const larguraAtual = parseFloat(document.getElementById('barra-cheia').style.width);
 
-  i.replaceWith(input);
-  input.focus();
-  input.select();
+  if (larguraAtual > 100) {
+    document.getElementById('barra-cheia').style.width = '100%';
+    document.getElementById('total-%').innerText = "Error % > 100%";
+    document.getElementById('howmany').innerText = "Error";
+
+  }
 }
+
+function atualizacoes(){
+  add_total_adimissoes();
+  add_total_func();
+}
+
+// function porcentage_totalF() {
+//     const total_txt = document.getElementById('total').innerText;
+//     const howmanyvar = document.getElementById('howmany').innerText;
+//     let now_porcentage = (howmanyvar * 100) / total_txt;
+
+//     document.getElementById('total-%').innerText = now_porcentage.toFixed(1) + '%';
+//     document.getElementById('barra-cheia').style.width = now_porcentage + '%';
+// }
+
+// function admissao() {
+//   let total = Number(document.getElementById('total').innerText);
+//   let mes = Number(document.getElementById('mes').innerText);
+
+//   let resultado = (mes / total) * 100;
+//   document.getElementById('admissao').innerText = '+' + resultado.toFixed(1) + '%';
+//   let barra = document.getElementById('barrames').style.width = resultado + '%';
+// }
+
+// function desligamentos(){
+//   let admissoes = Number(document.getElementById('mes').innerText);
+//   let desliga = Number(document.getElementById('deslig').innerText);
+//   let total = Number(document.getElementById('total').innerText);
+
+//   let saldo = admissoes - desliga;
+//   let now_porcen = (desliga / total) * 100;
+
+//   document.getElementById('saldo').innerText = '+' + saldo.toFixed(0);
+//   document.getElementById('barradeslig').style.width = now_porcen + '%';
+// }
+
+// function editar(i) {
+//   const valorAtual = i.textContent;
+//   const input = document.createElement('input');
+//   input.value = valorAtual;
+//   input.className = i.className;
+
+//   input.onblur = () => {
+//     const novoValor = input.value || valorAtual;
+//     i.textContent = novoValor;
+//     input.replaceWith(i);
+
+//     // if (i.id === 'total') {
+//     //   total = Number(novoValor); // usa novoValor, não input.value
+//     //   porcentage_totalF();               // recalcula depois de atualizar total
+//     // }
+//     // if (i.id === 'mes') {       // edita 'mes', não 'admissao'
+//     //   mes = Number(novoValor);
+//     //   admissao();               // recalcula e atualiza o campo admissao
+//     // }
+//     // if(i.id === 'deslig'){
+//     //   desligamentos();
+//     // }
+//   };
+
+
+//   input.onkeydown = (e) => {
+//       if (e.key === "Enter") input.blur();
+//   };
+
+//   i.replaceWith(input);
+//   input.focus();
+//   input.select();
+// }
+
+setInterval(() => {
+  atualizar_total_func_board();
+  atualizar_board_admissao();
+}, 500);
 
 window.onload = function(){
-  admissao();
-  porcentage_totalF();
-  desligamentos();
+  // admissao();
+  // porcentage_totalF();
+  // desligamentos();
 }
