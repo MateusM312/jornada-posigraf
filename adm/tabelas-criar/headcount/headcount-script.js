26016308377bb9d58c896be5dbee704520b19fca
MateusM312 <mateusmendes2626@gmail.com>
2026-04-25 15:25:57 -0300

    Organizei código headcount

--- adm/tabelas-criar/headcount/headcount-script.js
+++ adm/tabelas-criar/headcount/headcount-script.js
@@ -11,6 +11,8 @@ function close_sidebar() {
   document.getElementById("openav").style.display = "inline-block";
 }
 
+// --------------------------------------------------------------SOMAS-------------------------------------------------
+
 let total_func, ativos_total, admissoes_total;
 
 // Soma o total de funcionarios e ativos
@@ -42,6 +44,8 @@ function add_total_adimissoes(){
 
   admissoes_total = total_admissoes;
 }
+
+// --------------------------------------------------------------Atualizaçoes-------------------------------------------------
 
 // Atualiza com os dados somados
 function atualizar_board_admissao(){
@@ -84,75 +88,13 @@ function atualizar_total_func_board(){
   }
 }
 
+// --------------------------------------------------------------Outros-------------------------------------------------
+
 // É chamado quando o botão de atualização é apertado
 function atualizacoes(){
   add_total_adimissoes();
   add_total_func();
 }
-
-// function porcentage_totalF() {
-//     const total_txt = document.getElementById('total').innerText;
-//     const howmanyvar = document.getElementById('howmany').innerText;
-//     let now_porcentage = (howmanyvar * 100) / total_txt;
-
-//     document.getElementById('total-%').innerText = now_porcentage.toFixed(1) + '%';
-//     document.getElementById('barra-cheia').style.width = now_porcentage + '%';
-// }
-
-// function admissao() {
-//   let total = Number(document.getElementById('total').innerText);
-//   let mes = Number(document.getElementById('mes').innerText);
-
-//   let resultado = (mes / total) * 100;
-//   document.getElementById('admissao').innerText = '+' + resultado.toFixed(1) + '%';
-//   let barra = document.getElementById('barrames').style.width = resultado + '%';
-// }
-
-// function desligamentos(){
-//   let admissoes = Number(document.getElementById('mes').innerText);
-//   let desliga = Number(document.getElementById('deslig').innerText);
-//   let total = Number(document.getElementById('total').innerText);
-
-//   let saldo = admissoes - desliga;
-//   let now_porcen = (desliga / total) * 100;
-
-//   document.getElementById('saldo').innerText = '+' + saldo.toFixed(0);
-//   document.getElementById('barradeslig').style.width = now_porcen + '%';
-// }
-
-// function editar(i) {
-//   const valorAtual = i.textContent;
-//   const input = document.createElement('input');
-//   input.value = valorAtual;
-//   input.className = i.className;
-
-//   input.onblur = () => {
-//     const novoValor = input.value || valorAtual;
-//     i.textContent = novoValor;
-//     input.replaceWith(i);
-
-//     // if (i.id === 'total') {
-//     //   total = Number(novoValor); // usa novoValor, não input.value
-//     //   porcentage_totalF();               // recalcula depois de atualizar total
-//     // }
-//     // if (i.id === 'mes') {       // edita 'mes', não 'admissao'
-//     //   mes = Number(novoValor);
-//     //   admissao();               // recalcula e atualiza o campo admissao
-//     // }
-//     // if(i.id === 'deslig'){
-//     //   desligamentos();
-//     // }
-//   };
-
-
-//   input.onkeydown = (e) => {
-//       if (e.key === "Enter") input.blur();
-//   };
-
-//   i.replaceWith(input);
-//   input.focus();
-//   input.select();
-// }
 
 setInterval(() => {
   atualizar_total_func_board();
@@ -160,7 +102,5 @@ setInterval(() => {
 }, 500);
 
 window.onload = function(){
-  // admissao();
-  // porcentage_totalF();
-  // desligamentos();
+  console.log('Page Loaded!');
 }
